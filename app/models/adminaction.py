30b63dafb1bd2b4f7ba238d4cb9c90859d1c0e6a
banan141 <egorkusakin141@gmail.com>
2025-07-31 21:16:55 +0300

    Add migration

--- app/models/adminaction.py
+++ app/models/adminaction.py
@@ -1,15 +1,21 @@
 from sqlalchemy import Column, String, DateTime, Enum, ForeignKey
+import enum
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
 from datetime import datetime
 from app.database import Base
 
+class ActionType(enum.Enum):
+    CREATE = "create"
+    UPDATE = "update"
+    DELETE = "delete"
+
 class AdminAction(Base):
     __tablename__ = "AdminAction"
 
     id = Column(UUID(as_uuid=True), primary_key=True , default=uuid4)
-    admin_id = Column(UUID(as_uuid=True), ForeignKey("poka xz"))
+    admin_id = Column(UUID(as_uuid=True), ForeignKey("Client.id"))
     target_image_id = Column(UUID(as_uuid=True), ForeignKey("Image.id"))
-    action_type = Column(Enum)
+    action_type = Column(Enum(ActionType))
     reason = Column(String, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
--- app/models/client.py
+++ app/models/client.py
@@ -1,8 +1,13 @@
 from sqlalchemy import Column, String, Boolean, DateTime, Enum
 from sqlalchemy.dialects.postgresql import UUID
 from uuid import uuid4
+import enum
 from datetime import datetime
 from app.database import Base
+
+class RoleClient(enum.Enum):
+    ADMIN = "admin"
+    USER = "user"
 
 class Client(Base):
     __tablename__ = "Client"
@@ -10,7 +15,7 @@ class Client(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
     email = Column(String, unique=True, nullable=False)
     hashed_password = Column(String, nullable=False)
-    role = Column(Enum, nullable=False)
+    role = Column(Enum(RoleClient), nullable=False)
     is_active = Column(Boolean, nullable=False)
     registered_at = Column(DateTime, default=datetime.utcnow)
     oauth_provider = Column(String, nullable=False)
